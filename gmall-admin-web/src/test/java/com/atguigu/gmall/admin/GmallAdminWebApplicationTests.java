9c8216a1a6a02be83de405a475828d71d8fd9b2a
coffee <87863496@qq.com>
2020-02-12 12:39:15 +0800

    项目初始化完成

---
+++ gmall-admin-web/src/test/java/com/atguigu/gmall/admin/GmallAdminWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallAdminWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
